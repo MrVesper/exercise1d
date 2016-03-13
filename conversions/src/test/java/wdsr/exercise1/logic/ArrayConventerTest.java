54331c1064b9919346446e71ef76c123be47ddd8
MrVesper <j.papiniak@wp.pl>
2016-03-13 10:32:18 +0100

    Created ArrayConventerTest.java class with tests for ArrayConventer

---
+++ conversions/src/test/java/wdsr/exercise1/logic/ArrayConventerTest.java
@@ -0,0 +1,40 @@
+package wdsr.exercise1.logic;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import wdsr.exercise1.conversions.ArrayConverter;
+
+public class ArrayConventerTest {
+	private ArrayConverter arrayConverter;
+	
+	@Before
+	public void setup() {
+	 arrayConverter = new ArrayConverter();
+	}
+	
+	@Test(expected=NumberFormatException.class)
+	public void notIntegerTest_shouldRaiseAnNumberExceptionWhenArrayContainsValueWithIsNotAnInteger() {
+			String[] values = {"Jacek"};
+			arrayConverter.convertToInts(values);
+	}
+
+	@Test(expected=NullPointerException.class)
+	public void nullArgumentTest_shouldRaiseAnExceptionForNullArgument() {
+			String[] values = null;
+			arrayConverter.convertToInts(values);
+	}
+	
+	@Test(expected=NullPointerException.class)
+	public void nullIntArrayTest_shouldRaiseAnExceptionWhenCreatedIntArrayIsNull() {
+			String[] values = null;
+			int[] arr = arrayConverter.convertToInts(values);
+	}
+	
+	@Test(expected=NullPointerException.class)
+	public void emptyArgumentTest_shouldRaiseAnExceptionForEmptyArgument() {
+			String[] values = {};
+			arrayConverter.convertToInts(values);
+	}
+
+}
